931c0b85184ed456129edfacbfa5781923b17b88
David Perrenoud <david.perrenoud@ti8m.ch>
2018-03-01 12:16:48 +0100

    Update LICENSE

--- contracts/TendAssets.sol
+++ contracts/TendAssets.sol
@@ -1,12 +1,12 @@
 //! TendAssets contract
 //!
-//! Copyright 2018 David Perrenoud, ti&m AG
+//! Copyright 2018 Marco Abele, TEND Technologies
 //!
 //! Licensed under the Apache License, Version 2.0 (the "License");
 //! you may not use this file except in compliance with the License.
 //! You may obtain a copy of the License at
 //!
-//!         http://www.apache.org/licenses/LICENSE-2.0
+//!     http://www.apache.org/licenses/LICENSE-2.0
 //!
 //! Unless required by applicable law or agreed to in writing, software
 //! distributed under the License is distributed on an "AS IS" BASIS,
--- contracts/asset/ExperienceToken.sol
+++ contracts/asset/ExperienceToken.sol
@@ -1,6 +1,6 @@
 //! ExperienceToken contract
 //!
-//! Copyright 2018 David Perrenoud, ti&m AG
+//! Copyright 2018 Marco Abele, TEND Technologies
 //!
 //! Licensed under the Apache License, Version 2.0 (the "License");
 //! you may not use this file except in compliance with the License.
